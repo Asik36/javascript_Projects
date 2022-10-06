c90fe78955b6cdd4d184f0215c9557a5b8ce2834
Asik36 <asik.shick@gmail.com>
2022-10-06 23:35:13 +0300

    something

--- something-new/index.js
+++ something-new/index.js
@@ -28,12 +28,9 @@ document.addEventListener('keydown', function(event) {
     switch(event.keyCode){
         case 87: // w key
             if(bool_up){
-                hit.cloneNode(true).play();
-                document.getElementById("dot_up").style.background = "green";
+                noteHit("dot_up");
             } else{
-                miss.cloneNode(true).play();
-                document.getElementById("dot_up").style.background = "red";
-
+                noteMiss("dot_up");
             }
         break;
         
@@ -86,30 +83,83 @@ var hit = new Audio("hit.wav"); // buffers automatically when created
 var miss = new Audio("hit2.wav"); // buffers automatically when created
 
 
-function song(){
-    
+
+
+
+
+function noteMiss(dot) {
+    miss.cloneNode(true).play();
+    document.getElementById(dot).style.background = "red";
 }
 
-function remove() {
-    var elem = document.getElementById('btn_start');
-    elem.parentNode.removeChild(elem);
-    return false;
+function noteHit(dot) {
+    hit.cloneNode(true).play();
+    document.getElementById(dot).style.background = "green";
 }
 
-function notes(dirc,sec){
-    var elem = document.createElement("div");
-    elem.classList.add("Note");
+function noteUp(sec){
+    var elem = newNote("up", sec);
+    startAnimation("up", sec,elem);
+
     elem.addEventListener('animationend', () => {
-        bool_up = false;
         elem.remove();
+        bool_up = false;
     });
+}
+function noteLeft(sec){
+    var elem = newNote("left", sec);
+    startAnimation("left", sec,elem);
+    elem.addEventListener('animationend', () => {
+        elem.remove();
+        bool_left = false;
+    });
+}
+function noteRight(sec){
+    var elem = newNote("right", sec);
+    startAnimation("right", sec,elem);
+    elem.addEventListener('animationend', () => {
+        elem.remove();
+        bool_right = false;
+    });
+}
+function noteDown(sec){
+    var elem = newNote("down", sec);
+    startAnimation("down", sec,elem);
+    elem.addEventListener('animationend', () => {
+        elem.remove();
+        bool_down = false;
+    });
+}
 
-    document.getElementById('dot_'+dirc).append(elem);
+
+
+
+
+
+function startAnimation(dirc,sec,elem){
+    switch(dirc){
+        case 'left':
+            setTimeout(() =>{bool_left=true},sec*1000-500);
+        case 'right':
+            setTimeout(() =>{bool_right=true},sec*1000-500);
+        case 'up':
+            setTimeout(() =>{bool_up=true},sec*1000-500);
+        case 'down':
+            setTimeout(() =>{bool_down=true},sec*1000-500);
+
+    }
     elem.style.animation = dirc+" "+sec+"s linear";
-    setTimeout(() =>{bool_up=true},sec*1000-500);
+    setTimeout(() =>{bool_up=true},sec*1000-100);
     console.log(sec*1000);
 }
-notes("up" ,3);
-notes("down" ,4);
-notes("right" ,5);
-notes("up" ,6);
+
+function newNote(dirc){
+    var elem = document.createElement("div");
+    elem.classList.add("Note");
+    document.getElementById('dot_'+dirc).append(elem);
+    return elem;
+}
+noteUp(1.6);
+noteUp(1.4);
+noteUp(1.2);
+noteUp(1.0);
---
+++ something-new/test.js
@@ -0,0 +1,7 @@
+var boolean = true;
+function whatever(bool){
+    bool = false;
+
+}
+whatever(boolean);
+console.log(boolean);
